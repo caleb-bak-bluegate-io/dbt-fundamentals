fc5d6ba4770b77afa275dff05b74d50ae997e7d2
caleb.bak@bluegate.io <caleb.bak@bluegate.io>
2022-03-20 11:40:17 +0000

    Created a singular test for total_amount in stg_payments

---
+++ target/run/jaffle_shop/tests/assert_positive_total_for_payments.sql
@@ -0,0 +1,19 @@
+select
+      count(*) as failures,
+      count(*) != 0 as should_warn,
+      count(*) != 0 as should_error
+    from (
+      with
+
+payments as (
+    select * from analytics.dbt_dbtakeehuna.stg_payments
+)
+
+select
+    order_id,
+    sum( amount ) as total_amount
+from payments
+group by 1
+having not ( total_amount >= 0 )
+      
+    ) dbt_internal_test
